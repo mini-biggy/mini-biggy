e419d6a9d7f435e92fc9592dad016f5e1d5694f9
Namaneo <Namaneo>
2018-10-06 14:05:10 +0200

    Use Timer instead of infinite loop in DailyBackup

--- Mini-Biggy/BackupStrategies/DailyBackup.cs
+++ Mini-Biggy/BackupStrategies/DailyBackup.cs
@@ -1,37 +1,41 @@
 ﻿using System;
-using System.Threading.Tasks;
-using MiniBiggy.Util;
+using System.Threading;
 
-namespace MiniBiggy.BackupStrategies {
-    public class DailyBackup : IBackupStrategy {
+namespace MiniBiggy.BackupStrategies
+{
+    public class DailyBackup : IBackupStrategy
+    {
         private readonly int _hourBase;
         private FileSystemBackup _backup;
+        private Timer _timer;
 
         public event EventHandler<BackupAttemptedEventArgs> BackupAttempted;
 
-        public DailyBackup(int hourBase, string listPath, string pathToSaveTheBackup, int maxNumberOfFilesToKeep = 10) {
+        public DailyBackup(int hourBase, string listPath, string pathToSaveTheBackup, int maxNumberOfFilesToKeep = 10)
+        {
             _hourBase = hourBase;
             _backup = new FileSystemBackup(listPath, pathToSaveTheBackup, maxNumberOfFilesToKeep);
         }
 
-        public void Start() {
-            Task.Factory.StartNew(Loop, TaskCreationOptions.LongRunning);
+        public void Start()
+        {
+            var startAt = TimeSpan.FromHours(_hourBase) - DateTime.Now.TimeOfDay;
+            if (startAt.Ticks < 0)
+                startAt += TimeSpan.FromDays(1);
+
+            _timer = new Timer(state => Loop(), null, startAt, TimeSpan.FromDays(1));
         }
 
-        private async Task Loop() {
-            while (true) {
-                await TimeMachine.Delay(TimeSpan.FromMinutes(1));
-                var currentHour = TimeMachine.Now.Hour;
-                if (currentHour != _hourBase) {
-                    return;
-                }
-                try {
-                    var backupPath = _backup.Backup();
-                    BackupAttempted?.Invoke(this, new BackupAttemptedEventArgs(backupPath));
-                }
-                catch (BackupException ex) {
-                    BackupAttempted?.Invoke(this, new BackupAttemptedEventArgs(ex));
-                }
+        private void Loop()
+        {
+            try
+            {
+                var backupPath = _backup.Backup();
+                BackupAttempted?.Invoke(this, new BackupAttemptedEventArgs(backupPath));
+            }
+            catch (BackupException ex)
+            {
+                BackupAttempted?.Invoke(this, new BackupAttemptedEventArgs(ex));
             }
         }
     }
